adfca02559ec141deae10f02dd47a7e5afcbc8fe
ace20022 <ace20022@ymail.com>
2015-11-29 22:00:46 +0100

    DVDDemuxCDDA

--- xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxCDDA.cpp
+++ xbmc/cores/VideoPlayer/DVDDemuxers/DVDDemuxCDDA.cpp
@@ -115,7 +115,7 @@ DemuxPacket* CDVDDemuxCDDA::Read()
   }
 
   pPacket->iSize = m_pInput->Read(pPacket->pData, CDDA_READ_SIZE);
-  pPacket->iStreamId = 0;
+  pPacket->iStreamId = m_stream->iId;
 
   if(pPacket->iSize < 1)
   {
@@ -166,13 +166,26 @@ int CDVDDemuxCDDA::GetStreamLength()
   return (int)track_mseconds;
 }
 
-CDemuxStream* CDVDDemuxCDDA::GetStream(int iStreamId)
+CDemuxStream* CDVDDemuxCDDA::GetStream(int64_t iStreamId)
 {
-  if(iStreamId != 0)
+  if(!m_stream || (iStreamId != m_stream->iId))
     return NULL;
 
   return m_stream;
 }
+
+const std::vector<CDemuxStream*> CDVDDemuxCDDA::GetStreams() const
+{
+  std::vector<CDemuxStream*> streams;
+
+  if (m_stream != nullptr)
+  {
+    streams.push_back(m_stream);
+  }
+
+  return streams;
+}
+
 
 int CDVDDemuxCDDA::GetNrOfStreams()
 {
@@ -187,8 +200,8 @@ std::string CDVDDemuxCDDA::GetFileName()
     return "";
 }
 
-void CDVDDemuxCDDA::GetStreamCodecName(int iStreamId, std::string &strName)
+void CDVDDemuxCDDA::GetStreamCodecName(int64_t iStreamId, std::string &strName)
 {
-  if (m_stream && iStreamId == 0)
+  if (m_stream && iStreamId == m_stream->iId)
     strName = "pcm";
 }
